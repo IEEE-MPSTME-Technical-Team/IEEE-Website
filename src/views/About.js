8840afd6855a00edd1924ab70f3040dab1e8d8c7
Nikhil-Chouhan <Nikhil-Chouhan@users.noreply.github.com>
2020-12-25 01:52:34 +0530

    commit

--- src/views/About.js
+++ src/views/About.js
@@ -11,7 +11,7 @@ const About = () => {
             <div>
                 <div className="mainLand">
                     <Grid container spacing={40}>
-                        <Grid item xs={12} style={{ marginBottom: 70}}  className="landingContent">
+                        <Grid item xs={12} style={{ marginBottom: 70}}  className="aboutContent">
                             <AboutMain />
                         </Grid>
                         <Grid item xs={12} className="nav">
